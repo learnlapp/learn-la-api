b69ac692851f1d4385911b767c79398581ea185f
Thomas Lai <laiyiuki@gmail.com>
2018-08-16 15:27:16 +0800

    bug fixes on generate initial logs

--- src/services/matchings/hooks/after/init-log-msgs.js
+++ src/services/matchings/hooks/after/init-log-msgs.js
@@ -1,17 +1,32 @@
 module.exports = function initLogMsgs() {
   return async context => {
-    const { _id } = context.result;
+    const { _id, type } = context.result;
+
+    let from;
+    if (type === 'course') {
+      from = 'student';
+    }
+
+    if (type === 'student') {
+      from = 'teacher';
+    }
 
     const matchingLogs = [
-      { matchingId: _id, to: 'student', logId: 'invitationMsg' },
-      { matchingId: _id, to: 'teacher', logId: 'invitationMsg' },
+      {
+        matchingId: _id,
+        from,
+        to: 'student',
+        logId: 'invitationMsg',
+      },
+      {
+        matchingId: _id,
+        from,
+        to: 'teacher',
+        logId: 'invitationMsg',
+      },
     ];
-    console.log('start sending');
-    // context.app.service('matching-logs').create(matchingLogs);
 
     const res = await context.app.service('matching-logs').create(matchingLogs);
-    console.log('res', res);
-    // context.abc = res;
     return context;
   };
 };
