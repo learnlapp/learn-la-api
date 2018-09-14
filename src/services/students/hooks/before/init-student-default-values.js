cb0c8b11fd17dbb80980dd87fc2d2a160853b7ef
Thomas Lai <laiyiuki@gmail.com>
2018-09-14 18:15:12 +0800

    bbug fixes

--- src/services/students/hooks/before/init-student-default-values.js
+++ src/services/students/hooks/before/init-student-default-values.js
@@ -3,11 +3,11 @@ const { NotFound } = require('@feathersjs/errors');
 module.exports = function initTeacherQuota() {
   return async context => {
     const studentSettings = context.app.get('appSettings').student;
-    const { freeApplyfreeApplyQuotaQuota } = studentSettings;
+    const { freeApplyfreeApplyQuota } = studentSettings;
 
     context.data = {
       ...context.data,
-      freeApplyQuotaLeft: freeApplyfreeApplyQuotaQuota,
+      freeApplyQuotaLeft: freeApplyfreeApplyQuota,
     };
     return context;
   };
