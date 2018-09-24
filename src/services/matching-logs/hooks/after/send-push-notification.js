19b5d08762a34323adf348503186bf1f2d6d9467
Thomas Lai <laiyiuki@gmail.com>
2018-09-24 12:51:54 +0800

    fix bug on create matching

--- src/services/matching-logs/hooks/after/send-push-notification.js
+++ src/services/matching-logs/hooks/after/send-push-notification.js
@@ -4,13 +4,20 @@ const verificationTypes = require('../../../../modules/verification-types');
 
 module.exports = function sendPushNotification() {
   return async context => {
-    const { logId, matching, matchingId, from, to, extra } = context.result;
+    const { logId, matchingId, from, to, extra } = context.result;
 
+    let matching = context.result.matching;
     if (!matching) {
-      console.error(
-        'matching not found, unable to send push notification to user.'
-      );
-      return context;
+      matching = await context.app
+        .service('matchings')
+        .get(matchingId, { fastJoinQuery: { student: 1, teacher: 1 } });
+
+      if (!matching.length) {
+        console.error(
+          'matching not found, unable to send push notification to user.'
+        );
+        return context;
+      }
     }
 
     const config = context.app.get('oneSignal')[to];
